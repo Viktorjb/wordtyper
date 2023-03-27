0dfa8b130d46e9aae99dee62901b5ae35782ce22
Viktorjb <vjagebrant@gmail.com>
2023-03-27 16:03:08 +0200

    Initial Commit

---
+++ WordTyper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordTyper
+//
+//  Created by Viktor on 2023-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WordTyper/WordLists.swift
@@ -0,0 +1,16 @@
+//
+//  WordLists.swift
+//  WordTyper
+//
+//  Created by Viktor on 2023-03-27.
+//
+
+import Foundation
+
+class WordLists {
+    
+    private let easyList = ["crab", "cheese", "rocket", "hair"]
+    private let hardList = ["shortcut", "preferences", "chocolate", "desktop", "cognitive"]
+    
+}
+
