b8b0d3e11ac425cd554693b3005e8d9571ac4200
Viktorjb <vjagebrant@gmail.com>
2023-03-27 16:47:04 +0200

    more model and comments

--- WordTyper/WordLists.swift
+++ WordTyper/WordLists.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 
+//Model class containing word lists and logic surrounding it
 class WordLists {
     
+    //Word lists
     private let easyList = ["crab", "cheese", "rocket", "hair"]
     private let hardList = ["shortcut", "preferences", "chocolate", "desktop", "cognitive"]
     
+    //gameList, to be used when a game is active
     private var gameList : [String] = []
     
+    //Fill up the gamelist with the easy words
     func setUpEasyGame(){
         gameList = easyList
     }
     
+    //Fill up the gamelist with the hard words
     func setUpHardGame(){
         gameList = hardList
     }
