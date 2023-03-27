e75f85fd921214d4b7fb4879786617f0e457d8e2
Viktorjb <vjagebrant@gmail.com>
2023-03-27 20:00:31 +0200

    structural change

--- WordTyper/ViewController.swift
+++ WordTyper/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var gamePlayWordLabel: UILabel!
+    
+    
+    @IBAction func gamePlayEnterText(_ sender: UITextField) {
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let model = WordLists()
+        model.setUpHardGame()
+        gamePlayWordLabel.text = model.getTopWord()
+        
     }
-
-
+    
 }
 
